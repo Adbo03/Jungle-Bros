1204c2cd1f5a367988189bea6a503d2d4d91bbb2
Adbo03 <96348053+Adbo03@users.noreply.github.com>
2025-01-24 12:29:06 +0100

    Translation of the comments

--- ennemis.c
+++ ennemis.c
@@ -1,11 +1,11 @@
-#include "ennemis.h"   // Inclut les déclarations et la structure Ennemi
+#include "ennemis.h"   
 #include "stdio.h"    
 
-// Initialise un ennemi avec ses propriétés de base
+/* Initialization of an ennemy */
 Ennemi* initEnnemi(SDL_Renderer* ecran, int type) {
     Ennemi *ennemi = (Ennemi*) malloc(sizeof(Ennemi));
 
-    /* Chargement des textures pour l'animation et initialisation des variables pour la gestion de l'ennemi */
+    /* Loading of the textures and initialization of the variables to manage the ennemy */
     if (type == TYPE_OISEAU){ 
         ennemi->textureCount = 4; 
         ennemi->textures = malloc(ennemi->textureCount * sizeof(SDL_Texture*));
@@ -50,31 +50,31 @@ Ennemi* initEnnemi(SDL_Renderer* ecran, int type) {
 
 void dessine_Ennemis(Ennemi* ennemi, SDL_Renderer* ecran) {
 
-    /* Mise à jour de la variable SDL_Rect pour l'affichage */
+    /* Update of the SDL_Rect variable for the display */
     ennemi->rect.x = ennemi->x - ennemi->rect.w / 2;
     ennemi->rect.y = ennemi->y - ennemi->rect.h / 2;
 
-    /* Affichage de l'ennemi a l'ecran */
+    /* Display of the ennemy on screen */
     SDL_RenderCopy(ecran, ennemi->textures[ennemi->currentFrame], NULL, &(ennemi->rect));
 }
 
-/* Met à jour les coordonnees et des etats des ennemis */
+/* Update the position and the state (alive/dead) of the ennemies */
 void updateEnnemi(Ennemi* ennemi[MAX_ENNEMIS], double elapsed, Effets_sonores* musique, Caverne cave, Obstacle obs) {
 
-    /* Mise à jour des positions de TOUS les ennemis */
+    /* Update the positions of ALL ennemies */
     for(int n = 0; n < MAX_ENNEMIS; n++){
-        double moveDistance = ennemi[n]->speed * (elapsed / 1000.0); // Calcule la distance de déplacement
+        double moveDistance = ennemi[n]->speed * (elapsed / 1000.0); // Calculation of the speed of movement
         
         if (ennemi[n]->isAlive) {
             
-            /* Mise à jour de la position de l'oiseau */
+            /* Update the position of the eagle */
             if (ennemi[n]->type == TYPE_OISEAU) {
                 if (ennemi[n]->y < COORD_SOL) {
                     if(!Mix_Playing(2)) Mix_PlayChannel(2, musique->oiseau, 0);
-                    ennemi[n]->y += moveDistance ;   // Déplacement vertical
+                    ennemi[n]->y += moveDistance ;   
                     ennemi[n]->x -= moveDistance;
                     
-                    /* Si l'oiseau traverse un bloc */
+                    /* If the eagle goes through an obstacle */
                     ennemi[n]->rect.x = ennemi[n]->x - ennemi[n]->rect.w/2;
                     ennemi[n]->rect.y = ennemi[n]->y - ennemi[n]->rect.h/2;
                     obs.rect.x = obs.x - obs.rect.w/2;
@@ -86,28 +86,28 @@ void updateEnnemi(Ennemi* ennemi[MAX_ENNEMIS], double elapsed, Effets_sonores* m
                 } 
                 else {
                     ennemi[n]->x -= moveDistance;
-                    ennemi[n]->speed += 3.0;         // L'oiseau accélère
+                    ennemi[n]->speed += 3.0;         // The eagle speeds up
                 }
             }
 
-            /* Mise à jour de la position de l'araignée */
+            /* Update the position of the spider */
             else if (ennemi[n]->type == TYPE_ARRAIGNE) {
-                ennemi[n]->jumpTimer += elapsed / 1000.0; // Incrémente le temps écoulé
+                ennemi[n]->jumpTimer += elapsed / 1000.0; // Incrementation by the elapsed time
                 ennemi[n]->x -= moveDistance;
 
                 if (ennemi[n]->isJumping) {
-                    /* Calcul du mouvement en cloche (utilise un sinus pour le saut) */
-                    float progress = ennemi[n]->jumpTimer / 1.5; // Durée totale du saut
+                    /* Calculation for the jump (using a sinus) */
+                    float progress = ennemi[n]->jumpTimer / 1.5; // Total duration of a jump
                     if (progress > 1.0) {
-                        // Fin du saut
+                        // End of the jump
                         ennemi[n]->isJumping = 0;
                         ennemi[n]->jumpTimer = 0;
                     } else {
                         // Position pendant le saut
-                        ennemi[n]->y = SCREEN_HEIGHT - (ennemi[n]->rect.h/2 + 15) - ennemi[n]->jumpHeight * sin(progress * PI); // Mouvement vertical
+                        ennemi[n]->y = SCREEN_HEIGHT - (ennemi[n]->rect.h/2 + 15) - ennemi[n]->jumpHeight * sin(progress * PI); // Vertical movement
                     }
                     
-                    /* Si l'arraignee traverse un bloc */
+                    /* If the spider goes through an obstacle */
                     ennemi[n]->rect.x = ennemi[n]->x - ennemi[n]->rect.w/2;
                     ennemi[n]->rect.y = ennemi[n]->y - ennemi[n]->rect.h/2;
                     obs.rect.x = obs.x - obs.rect.w/2;
@@ -118,7 +118,7 @@ void updateEnnemi(Ennemi* ennemi[MAX_ENNEMIS], double elapsed, Effets_sonores* m
                 } 
                 else if (ennemi[n]->jumpTimer >= 3.0) {
 
-                    /* Déclenchement d'un nouveau saut */
+                    /* Unleashing of a new jump */
                     ennemi[n]->isJumping = 1;
                     ennemi[n]->jumpTimer = 0;
                     ennemi[n]->jumpStartX = ennemi[n]->x;
@@ -127,7 +127,7 @@ void updateEnnemi(Ennemi* ennemi[MAX_ENNEMIS], double elapsed, Effets_sonores* m
             }
         } 
         
-        /* Vérifie si l'ennemi sort de l'écran */
+        /* Check if the ennemy is still on screen */
         if ((ennemi[n]->x + ennemi[n]->rect.w / 2) < 0) {
             ennemi[n]->x = (SCREEN_WIDTH) + (ennemi[n]->rect.w/2);
             if(ennemi[n]->type == TYPE_OISEAU) ennemi[n]->y = SCREEN_HEIGHT/2 - ennemi[n]->rect.h/2; 
@@ -138,7 +138,7 @@ void updateEnnemi(Ennemi* ennemi[MAX_ENNEMIS], double elapsed, Effets_sonores* m
         if(!check_Alive(ennemi) && !cave.move && !cave.on_screen && (!ennemi[n]->isAlive)){
             int luck = rand()%100;
 
-            /* Apparition aleatoire des ennemis */
+            /* Random appearances of the ennemy */
             if(((ennemi[n]->type == TYPE_OISEAU) && ( luck <= 1)) || ((ennemi[n]->type == TYPE_ARRAIGNE) && ( luck <= 3))){
                 ennemi[n]->isAlive=TRUE;
                 ennemi[n]->speed_start += 25;
@@ -148,7 +148,7 @@ void updateEnnemi(Ennemi* ennemi[MAX_ENNEMIS], double elapsed, Effets_sonores* m
     }
 }
 
-/* Verifie si un ennemi est affiché à l'ecran */
+/* Check if the ennemy is still on screen */
 int check_Alive(Ennemi* ennemis[MAX_ENNEMIS]){
     for(int n = 0; n < MAX_ENNEMIS; n++){
         if(ennemis[n]->isAlive)  return TRUE;
@@ -159,10 +159,10 @@ int check_Alive(Ennemi* ennemis[MAX_ENNEMIS]){
 
 void updateAnimation(Ennemi* ennemi) {
     if (ennemi->textureCount > 0) {
-        // Incrémente le compteur
+        // Incrementation of the counter
         ennemi->animationCounter++;
 
-        // Si le délai est atteint, met à jour le cadre
+        // If the time is up, update the frame
         if (ennemi->animationCounter >= ennemi->animationDelay) {
             ennemi->currentFrame = (ennemi->currentFrame + 1) % ennemi->textureCount;
             ennemi->animationCounter = 0;  // Réinitialise le compteur
@@ -170,7 +170,7 @@ void updateAnimation(Ennemi* ennemi) {
     }
 }
 
-/* Destruction des textures et desallocation de la mémoire allouée */
+/* Destruction of the textures and free of the allocated memory */
 void free_ennemis(Ennemi** ennemi) {
 
     if ((*ennemi)->textures != NULL) { 
