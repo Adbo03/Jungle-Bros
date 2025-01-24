0121f8fbf43923f7339137b12172bd05b7ad17c8
Adbo03 <96348053+Adbo03@users.noreply.github.com>
2025-01-24 17:23:41 +0100

    Translation of the comments

--- ennemis.h
+++ ennemis.h
@@ -9,41 +9,41 @@
 #include "obstacle.h"
 #endif // _OBSTACLE_H
 
-// Structure représentant un ennemi
+// Structure of an enemy
 typedef struct _Ennemi{
     SDL_Rect rect;
-    SDL_Texture** textures;     // Tableau dynamique de textures
-    int textureCount;           // Compte le nombre de textures necessaire pour l'animation de l'ennemis
-    float x, y;                 // Position de l'ennemi
-    float speed;                // Vitesse de déplacement de l'ennemi
+    SDL_Texture** textures;     // Dynamic tab of textures
+    int textureCount;           // Count the number of textures required for the animation of the enemy
+    float x, y;                 // Position of the enemy
+    float speed;                // Movement speed of the enemy
     float speed_start;
-    int isAlive;                // État de l'ennemi : 1 = vivant (affiché), 0 = mort (hors de l'ecran)
-    int type;                   // Type de l'ennemi pour des comportements différents
-    int currentFrame;           // Frame actuel
-    int animationDelay;         // Délai en nombre d'itérations
-    int animationCounter;       // Compteur pour gérer le délai
-    double jumpTimer;           // Temps écoulé depuis le dernier saut
-    int isJumping;              // Indique si l'araignée est en train de sauter
-    double jumpStartX;          // Position X de départ pour le saut
-    double jumpHeight;          // Hauteur maximale du saut
+    int isAlive;                // State of the enemy : 1 = alive (display), 0 = dead (out of screen)
+    int type;                   // Type of enemy (spider or bird)
+    int currentFrame;           // current frame for the display
+    int animationDelay;         // Delay (number of loops)
+    int animationCounter;       // Counter to manage the delay
+    double jumpTimer;           // elapsed time since the last jump
+    int isJumping;              // Indicates if the spider is currently jumping
+    double jumpStartX;          // X position at the beginning of a jump
+    double jumpHeight;          // Maximal height for a jump
 } Ennemi;
 
-// Initialise un ennemi avec ses paramètres de base
+// Initialize an enemy with it's parameters by default
 Ennemi* initEnnemi(SDL_Renderer* ecran, int type);
 
-// Met à jour la position et l'état de l'ennemi
+// Update the position and state of an enemy
 void updateEnnemi(Ennemi* ennemi[MAX_ENNEMIS],double elapsed,Effets_sonores* musique, Caverne cave, Obstacle obs);
 
-/* Dessine les ennemis */
+// Display of the enemy 
 void dessine_Ennemis(Ennemi* ennemi,SDL_Renderer* ecran);
 
-//Change l'image de l'ennemi à chaque appel
+// Change the image of the enemy at every call (movement effect)
 void updateAnimation(Ennemi* ennemi);
 
-/* Verifie si un ennemi est affiché à l'ecran */
+// Check if an enemy is displayed on screen
 int check_Alive(Ennemi* ennemis[MAX_ENNEMIS]);
 
-/* Desalloue les textures et l'ennemi */
+// Destroys the textures and free the memory allocated for the enemy
 void free_ennemis(Ennemi** ennemi);
 
 #endif // _ENNEMI_H
