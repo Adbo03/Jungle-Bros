868aea405767f123b406107b08b5a38034df2edc
Adbo03 <96348053+Adbo03@users.noreply.github.com>
2025-01-24 18:25:42 +0100

    Translation of the comments

--- main.c
+++ main.c
@@ -18,21 +18,21 @@ int main(int argc, char** argv)
 
    success=init(&fenetre,&ecran,"Jungle Bros.");
 
-   /* Initialisation de SDL_Mixer */
+   /* Initialization of SDL_Mixer */
     if (Mix_OpenAudio(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 1024) < 0){
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Erreur initialisation SDL_mixer : %s", Mix_GetError());
         SDL_Quit();
         return -1;
     }
 
-    /* Initialisation de SDL_TTF */
+    /* Initialization of SDL_TTF */
     if(TTF_Init() < 0){
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Erreur initialisation SDL_TTF : %s", TTF_GetError());
         SDL_Quit();
         return -1;
     }
 
-    /* Initialisation des effets sonores */
+    /* Initialization of the sound effects */
     Effets_sonores musique;
     Init_musique(&musique);
     
@@ -41,25 +41,25 @@ int main(int argc, char** argv)
     }
     
     else{
-	    /* ------ INITIALISATION ------ */
+	/* ------ INITIALIZATION ------ */
 
-        /* Allocation des canaux */
+        /* Allocation of the sound channels */
         Mix_AllocateChannels(3);
 
-        /* Reglages du volume des canaux */
-        Mix_Volume(0,128);     // Canal musique jeu/menu/fin de jeu
-        Mix_Volume(1,110);     // Canal marche et saut personnage
-        Mix_Volume(2,110);     // Canal bruit sonores(ennemis/personnage)
+        /* Channels volume control */
+        Mix_Volume(0,128);     // Channel for the game/menu/end game music
+        Mix_Volume(1,110);     // Channel for the walking and jumping sounds (character)
+        Mix_Volume(2,110);     // Channels for other sound effects (enemies/character)
 
 
-        /* Initialisation de l'arriere plan (defilement) */
+        /*Initialization of the background */
         Arriere_plan decor;
         Init_image(ecran,&decor.texture_1,&decor.fond_1,"Images/fond_super_mario_1.bmp");
         Init_image(ecran,&decor.texture_2,&decor.fond_2,"Images/fond_super_mario_2.bmp");
         Init_image(ecran,&decor.texture_3,&decor.fond_3,"Images/fond_super_mario_3.bmp");
         Init_image(ecran,&decor.texture_4,&decor.fond_4,"Images/fond_super_mario_4.bmp");
         
-        /* Initialisation du texte */
+        /* Initialization of the text */
         SDL_Rect name_rect, start_rect, exit_rect, name_rect_2;
         char game_str[14] = "Jungle Bros.", start_str[8] = " Start ",exit_str[7] = " Exit ";
         Init_police(ecran,game_str,&name_texture,"game_name",&name_rect);
@@ -67,46 +67,46 @@ int main(int argc, char** argv)
         Init_police(ecran,exit_str,&exit_texture,"exit",&exit_rect);
         Init_police(ecran,game_str,&name_texture_2,"game_name_2",&name_rect_2);
 
-        /* Initialisation des blocs (menu) */
+        /* Initialization of the bricks (menu) */
         Brick_menu blocs;
         Init_blocs(ecran,start_rect,exit_rect,&blocs);
 
-        /* Initialisation des images de fin de jeu (lose/win) */
+        /* Initialization of the end game images (lose/win) */
         End_game fin_jeu;
         Init_end_game(ecran,&fin_jeu);
         
-        /* Initialisation du personnage */
+        /* Initialization of the character */
         Personnage mario;
         Init_personnage(ecran, &mario);
 
-        /* Initialisation de l'obstacle */
+        /* Initialization of the obstacle */
         Obstacle obs;
         Init_obstacle(ecran,&obs);
 
-        /* Initialisation de la caverne (fin) */
+        /* Initialization of the cave (end) */
         Caverne cave;
         Init_cave(ecran, &cave);
 
-        /* Initialisation ennemis */
+        /* Initialization of the enemies */
         Ennemi* ennemi[MAX_ENNEMIS];
-        ennemi[0] = initEnnemi(ecran, TYPE_OISEAU);  // Initialise l'oiseau avec les paramètres appropriés
-        ennemi[1] = initEnnemi(ecran, TYPE_ARRAIGNE); // Initialise l'arraigné avec les paramètres appropriés
+        ennemi[0] = initEnnemi(ecran, TYPE_OISEAU);  // BIRD
+        ennemi[1] = initEnnemi(ecran, TYPE_ARRAIGNE); // SPIDER
 
-        /* Initialisation des variables */
+        /* Initialization of the game variables */
         int quit = FALSE, menu = TRUE;
         int wait = 0;
         int compt_endgame = 0;
 
-        /* Controle du temps pour les mises à jour */
+        /* Time control for the updates */
         double previous = getCurrentTimeMS();
         double lag = 0.0;
         double current = 0.0;
     
 
-        /* Lancement de la musique du menu */
+        /* Start of the game menu music */
         Mix_PlayChannel(0,musique.menu, -1);
 
-        /* BOUCLE DU JEU / GAME LOOP */
+        /* GAME LOOP */
         while( !quit ){
             current = getCurrentTimeMS();
             elapsed = current - previous;
@@ -116,20 +116,20 @@ int main(int argc, char** argv)
             
             if(!fin_jeu.win && !fin_jeu.lose && !menu && !mario.exit_menu && !mario.exit_game){
 
-                /* Deplacement personnage */
+                /* Movement of the character */
                 control_keybord(ecran,&quit,&mario,&fin_jeu,musique,&obs, &cave, &decor, ennemi);
                 
-                /* Deplacement ennemis */
+                /* Movement of the ennemies */
                 updateEnnemi(ennemi,elapsed,&musique,cave,obs); 
                 
 
                 for (int i = 0; i < MAX_ENNEMIS; i++) {
                     updateAnimation(ennemi[i]);
 
-                    /* Si le personnage et l'ennemi sont toujours en vie */
+                    /* If the character and the enemy are still alive */
                     if (ennemi[i]->isAlive && mario.isAlive) { 
                         
-                        /* Le personnage meurt en cas de collision */
+                        /* The character dies if he touches an enemy */
                         mario.rect.x = mario.x - mario.rect.w/2;
                         mario.rect.y = mario.y - mario.rect.h/2;
                         ennemi[i]->rect.x = ennemi[i]->x - ennemi[i]->rect.w/2;
@@ -149,7 +149,7 @@ int main(int argc, char** argv)
             
             while (lag >= MS_PER_UPDATE){
 
-                /* ------ MISE A JOUR ------ */
+                /* ------ UPDATE ------ */
                 
                 lag -= MS_PER_UPDATE;
                 
@@ -158,22 +158,22 @@ int main(int argc, char** argv)
                     mario.alternance = FALSE;
                     menu_control(ecran,&blocs,&quit,&menu,musique);
 
-                    /* Clignotement du bouton selectionné */
+                    /* Blinking of the selected button */
                     if(blocs.selection_exit || blocs.selection_start)  blocs.clignotement = !blocs.clignotement; 
                     
-                    if(!menu && !quit && !mario.exit_menu){ /*  <=> Appui Start */
+                    if(!menu && !quit && !mario.exit_menu){ /*  <=> Start button selected */
                         mario.exit_menu = TRUE;
                         mario.dir = 'd';
 
-                        /* Lancement du son "ah ah" du personnage*/
+                        /* Playing of the "ah ah" character sound */
                         Mix_PlayChannel(2, musique.Here_we_go, 0);
 
-                        /* Arret de la musique du menu et lancement de celle du jeu */
+                        /* Stop of the menu music and playing the game music */
                         Mix_HaltChannel(0);
                         Mix_PlayChannel(0,musique.jeu, -1);
                     }
 
-                    /* Animation : le personnage sort du menu */
+                    /* Animation : the character exits the screen */
                     if(mario.exit_menu){
                         if(wait%2) mario.alternance = !mario.alternance;
                         wait++;
@@ -192,10 +192,10 @@ int main(int argc, char** argv)
                 } 
 
                 else{
-                    /* ------ GESTION DE LA PARTIE ------ */
+                    /* ------ GAME CONTROL ------ */
                     Mix_Volume(0,110);
 
-                    /* Animation : le personnage rentre dans la grotte (fin de partie) */
+                    /* Animation : the character enters the cave (end game) */
                     if(mario.exit_game){
                         mario.y = mario.previous_y;
                         if((mario.x - mario.rect.w/2) >= (cave.rect_d.x + cave.rect_d.w/2)){
@@ -210,7 +210,7 @@ int main(int argc, char** argv)
                         if(Mix_Playing(1)) Mix_HaltChannel(1);
                         if(Mix_Playing(2)) Mix_HaltChannel(2);
 
-                        /* Reinitialisation des elements du jeu */
+                        /* Reset of all the variables  */
                         if(((compt_endgame == 50) && fin_jeu.lose) || ((compt_endgame == 30) && fin_jeu.win)){
                             menu = TRUE;
                             compt_endgame = 0;
@@ -233,21 +233,21 @@ int main(int argc, char** argv)
                             decor.fond_4.x = 3*SCREEN_WIDTH/2;
                             mario.isAlive = TRUE;
 
-                            /* Oiseau */
+                            /* Bird */
                             ennemi[0]->x = SCREEN_WIDTH + 200;
                             ennemi[0]->y = SCREEN_HEIGHT/2 - ennemi[0]->rect.h/2;
                             ennemi[0]->isAlive = FALSE;
                             ennemi[0]->speed_start = 120;
                             ennemi[0]->speed = ennemi[0]->speed_start;
                             
-                            /* Arraigne */
+                            /* Spider */
                             ennemi[1]->x = SCREEN_WIDTH + 200 - ennemi[1]->rect.w/2;
                             ennemi[1]->y = COORD_SOL;
                             ennemi[1]->isAlive = FALSE;
                             ennemi[1]->speed_start = 100;
                             ennemi[1]->speed = ennemi[1]->speed_start;
                             
-                            /* Reprise de l'image personnage vivant */
+                            /* Reset of the image parameters of the character (dead --> alive) */
                             if(fin_jeu.lose){
                                 int rect_tmp;
                                 rect_tmp =  mario.rect.w;
@@ -259,14 +259,14 @@ int main(int argc, char** argv)
                             fin_jeu.win = FALSE;
                             fin_jeu.lose = FALSE;
                             
-                            /* Arret des musiques et lancement de celle du menu */
+                            /* Stoping all musics and sound effects and playing the game menu music*/
                             Mix_HaltChannel(-1);
                             Mix_PlayChannel(0,musique.menu, -1);
                         }
                     }
 
                     
-                    /* Effet sonore 'marche' non actif quand le personnage saute */
+                    /* When the character jumps, the walking sound effect must not be played */
                     if(!fin_jeu.lose && !fin_jeu.win && (mario.y == mario.previous_y) && ((mario.dir == 'g') || (mario.dir == 'd'))){ 
                         if(!Mix_Playing(1))
                             Mix_PlayChannel(1,musique.marche, -1);
@@ -280,35 +280,35 @@ int main(int argc, char** argv)
     
             }
 
-            /* ------ AFFICHAGE ------ */
+            /* ------ DISPLAY ------ */
 
             SDL_SetRenderDrawColor( ecran, 0xFF, 0xFF, 0xFF, 0xFF );
             SDL_RenderClear(ecran);
 
-            /* dessine l'image de fond */
+            /* Display of the background */
             dessine_fond(ecran, decor);
 
-            /* Affichage du menu d'acceuil */
+            /* Display of the game menu */
             if(menu){
                 if(blocs.clignotement){
                     if(blocs.selection_exit) dessine_rect_plein(ecran,&blocs.select_exit,White);
                     else if(blocs.selection_start){ dessine_rect_plein(ecran,&blocs.select_start,White); }
                 }
 
-                /* Affichage des briques */
+                /* Display of the bricks */
                 dessine_Img_redim(ecran,blocs.texture,blocs.brick_exit);
                 dessine_Img_redim(ecran,blocs.texture,blocs.brick_start);
                 
-                /* Affichage du texte */
+                /* Display of the text */
                 SDL_RenderCopy(ecran,name_texture_2,NULL,&name_rect_2);  
                 SDL_RenderCopy(ecran,name_texture,NULL,&name_rect);
                 SDL_RenderCopy(ecran,start_texture,NULL,&start_rect);
                 SDL_RenderCopy(ecran,exit_texture,NULL,&exit_rect);
             }
 
-            dessine_Img_redim(ecran,cave.texture_partie_gauche,cave.rect_g);        /* Caverne partie gauche*/
-            dessine_personnage(ecran,&mario);                                       /* Personnage */
-            dessine_Img_redim(ecran,cave.texture_partie_droite,cave.rect_d);        /* Caverne partie droite*/
+            dessine_Img_redim(ecran,cave.texture_partie_gauche,cave.rect_g);        /* Left part CAVE */
+            dessine_personnage(ecran,&mario);                                       /* Character */
+            dessine_Img_redim(ecran,cave.texture_partie_droite,cave.rect_d);        /* Right part CAVE*/
             dessine_obstacle(ecran,obs);                                            /* Obstacle */
             
             for (int i = 0; i < MAX_ENNEMIS; i++) {
@@ -326,33 +326,33 @@ int main(int argc, char** argv)
             SDL_RenderPresent(ecran);
         }
 
-        /* Desallocation mémoire */
+        /* Free the memory allocated */
         free_mario(&mario);
         for(int n = 0; n < MAX_ENNEMIS; n++)
             free_ennemis(&ennemi[n]);
         
         
-        /* Destruction des textures */
-        // Bouton de selection (menu)
+        /* Destruction of the textures */
+        // Selection button (menu)
         if(blocs.texture){SDL_DestroyTexture(blocs.texture); blocs.texture = NULL;}
         
         // Obstacle
         if(obs.texture){SDL_DestroyTexture(obs.texture); obs.texture = NULL;}
         
-        // Image de fin de jeu (win/game over)
+        // End game image (win/game over)
         if(fin_jeu.win_texture){SDL_DestroyTexture(fin_jeu.win_texture); fin_jeu.win_texture = NULL;}
         if(fin_jeu.lose_texture){SDL_DestroyTexture(fin_jeu.lose_texture); fin_jeu.lose_texture = NULL;}
         
-        // Caverne (fin de jeu)
+        // Cave (end game)
         if(cave.texture_partie_droite){SDL_DestroyTexture(cave.texture_partie_droite); cave.texture_partie_droite = NULL;}
         if(cave.texture_partie_gauche){SDL_DestroyTexture(cave.texture_partie_gauche); cave.texture_partie_gauche = NULL;}
         
-        // Arriere plan
+        //Background
         free_decor(&decor);
 	}
 
-    /* Destruction des textures */
-    //Texte
+    /* Destruction of the textures */
+    //Text
     if(name_texture){SDL_DestroyTexture(name_texture); name_texture = NULL;}
     if(start_texture){SDL_DestroyTexture(start_texture); start_texture = NULL;}
     if(exit_texture){SDL_DestroyTexture(exit_texture); exit_texture = NULL;}
@@ -361,12 +361,12 @@ int main(int argc, char** argv)
 
     TTF_Quit();
 
-    /* Libère les musiques et effets sonores */
+    /* Free the memory space allocated for musics and sound effects */
     Mix_HaltChannel(-1);  
     free_audio(&musique);
     Mix_CloseAudio();
 
-    /* Fermeture de l'ecran et de la fenetre */
+    /* Close the screen and window */
     close(&fenetre,&ecran);
     return 0;
 }
